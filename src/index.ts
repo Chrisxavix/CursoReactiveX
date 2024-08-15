e2ee2821c4098e4afd63dc9f3a5db96d5a7fbd33
Chrisxavix <chrischillogallid64@outlook.com>
2024-08-14 23:24:55 -0500

    upload observer create

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,31 @@
+import { Observable, Observer } from "rxjs";
+
+const obs$ = new Observable<string>( subs => {
+    subs.next("Hola");
+    subs.next("Mundo");
+    /* Forzar un error */
+    /* const a = undefined;
+    a.nombre = "Fernando"; */
+    subs.next("Brou");
+    subs.complete();
+    subs.next("Apoco");
+});
+
+const observer: Observer<any> = {
+    next: value => console.log("siguiente [next]: ", value),
+    error: error => console.log("error [obs]: ", error),
+    complete: () => console.log("COMPLETADO [obs]")
+}
 
 
-console.log('Hola Munsdo!');
+/* Se puede hacer */
+/* obs$.subscribe( console.log ); */
 
+/* Opción general */
+/* obs$.subscribe( 
+    valor => console.log("next", valor) ,
+    error => console.log("error", error),
+    () => console.info("completado")  
+); */
 
-
-
-
-
-
+obs$.subscribe(observer)
